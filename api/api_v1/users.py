f97ceba8d50319b52a49cafa563805f2d077f529
VarvarM <miha.furtat@yandex.ru>
2024-10-23 15:09:57 +0300

    added get from id for users

--- api/api_v1/users.py
+++ api/api_v1/users.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from core.models import db_helper
-from core.schemas.user import UserRead, UserCreate, UserUpdate, UserBase
-from crud.users import get_all_users
+
+from core.models import db_helper, User
+from core.schemas.user import UserRead, UserCreate, UserUpdate
 from crud import users as users_crud
 from crud.dependencies import user_by_id
 
@@ -11,11 +11,16 @@ router = APIRouter(tags=["Users"])
 
 @router.get('', response_model=list[UserRead])
 async def get_users(session: AsyncSession = Depends(db_helper.session_getter)):
-    users = await get_all_users(session=session)
+    users = await users_crud.get_all_users(session=session)
     return users
 
 
-@router.post('/create_user', response_model=UserRead)
+@router.get('/{user_id}')
+async def get_user(user: User = Depends(user_by_id)):
+    return user
+
+
+@router.post('/create_user', response_model=UserRead, status_code=status.HTTP_201_CREATED)
 async def create_user(user_create: UserCreate, session: AsyncSession = Depends(db_helper.session_getter)):
     user = await users_crud.create_user(session=session, user_create=user_create)
     return user
@@ -27,6 +32,6 @@ async def update_user(user_update: UserUpdate, user: User = Depends(user_by_id),
     return await users_crud.update_user(session=session, user_update=user_update, user=user)
 
 
-@router.delete('/{user_id}')
-async def delete_user(user: UserBase = Depends(user_by_id), session: AsyncSession = Depends(db_helper.session_getter)):
+@router.delete('/{user_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(user: User = Depends(user_by_id), session: AsyncSession = Depends(db_helper.session_getter)):
     return await users_crud.delete_user(session=session, user=user)
