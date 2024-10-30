c6d4d348aeb6f46f96882a81676466a293eb8766
VarvarM <miha.furtat@yandex.ru>
2024-10-30 14:15:33 +0300

    fixed url

--- api/__init__.py
+++ api/__init__.py
@@ -3,7 +3,5 @@ from fastapi import APIRouter
 from core.config import settings
 from .api_v1 import router as router_api_v1
 
-router = APIRouter(
-    prefix=settings.api.prefix,
-)
+router = APIRouter()
 router.include_router(router_api_v1)
